911dfdcc81a5caf578ae8864bce19066dae55d57
SALMAN <elfadili.salman@gmail.com>
2018-12-14 00:55:37 +0100

    Bootstrap crazy-ideas Spring boot app with JPA,Web,Rest,MongoDB

---
+++ src/main/java/com/crazyideas/CrazyIdeasServerApplication.java
@@ -0,0 +1,14 @@
+package com.crazyideas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrazyIdeasServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrazyIdeasServerApplication.class, args);
+	}
+
+}
+

dbbd859557b2c4dca17eddaea7bcf292c13f528f
SALMAN <elfadili.salman@gmail.com>
2018-12-15 01:05:35 +0100

    Change models/collections id field to generated ObjectId

--- src/main/java/com/crazyideas/repositories/IdeaRepository.java
+++ src/main/java/com/crazyideas/repositories/IdeaRepository.java
@@ -1,7 +1,8 @@
 package com.crazyideas.repositories;
 
 import com.crazyideas.models.Idea;
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface IdeaRepository extends MongoRepository<Idea, Long> {
+public interface IdeaRepository extends MongoRepository<Idea, ObjectId> {
 }
--- src/main/java/com/crazyideas/repositories/ThinkerRepository.java
+++ src/main/java/com/crazyideas/repositories/ThinkerRepository.java
@@ -1,9 +1,10 @@
 package com.crazyideas.repositories;
 
 import com.crazyideas.models.Thinker;
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface ThinkerRepository extends MongoRepository<Thinker, Long> {
+public interface ThinkerRepository extends MongoRepository<Thinker, ObjectId> {
 
-    public Thinker findByName(String name);
+    Thinker findByName(String name);
 }

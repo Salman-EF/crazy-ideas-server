33f99881289c4698c6fdfaa9b2e5f889f986471a
SALMAN <elfadili.salman@gmail.com>
2018-12-16 02:01:50 +0100

    Delete CommandLineRunner after successfully connected with MongoDB

--- src/main/java/com/crazyideas/CrazyIdeasServerApplication.java
+++ src/main/java/com/crazyideas/CrazyIdeasServerApplication.java
@@ -1,7 +1,18 @@
 package com.crazyideas;
 
+import com.crazyideas.models.Idea;
+import com.crazyideas.models.Thinker;
+import com.crazyideas.repositories.IdeaRepository;
+import com.crazyideas.repositories.ThinkerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class CrazyIdeasServerApplication {

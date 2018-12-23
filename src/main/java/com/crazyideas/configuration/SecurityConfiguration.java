dcfb1a45e968ad3ef1dcf68ce2c09f1692c18b13
SALMAN <elfadili.salman@gmail.com>
2018-12-23 01:23:08 +0100

    Build the spring boot security configuration class

--- src/main/java/com/crazyideas/configuration/SecurityConfiguration.java
+++ src/main/java/com/crazyideas/configuration/SecurityConfiguration.java
@@ -1,6 +1,6 @@
-package com.lambdasoft.risquesmanagement.configuration;
+package com.crazyideas.configuration;
 
-import javax.sql.DataSource;
+import com.crazyideas.services.ThinkerServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,61 +8,52 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
 import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 
 	@Autowired
-	private BCryptPasswordEncoder bCryptPasswordEncoder;
+	BCryptPasswordEncoder bCryptPasswordEncoder;
 
-	@Autowired
-	private DataSource dataSource;
-	
-	private final String USERS_QUERY = "select code, password, active from utilisateurs where code=upper(?)";
-	private final String ROLES_QUERY = "select u.code, r.role from utilisateurs u inner join utilisateur_role ur on (u.code = ur.utilisateur_code) inner join roles r on (ur.role_id=r.role_id) where u.code=upper(?)";
+	private static final String ADMIN = "ADMIN";
+    private static final String USER = "USER";
 
-	private final String USER = "USER";
-	private final String ADMIN = "ADMIN";
+	@Bean
+    UserDetailsService thinkerDetailsService(){
+	    return new ThinkerServices();
+    }
 	
 	@Override
  	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.jdbcAuthentication()
-			.usersByUsernameQuery(USERS_QUERY)
-			.authoritiesByUsernameQuery(ROLES_QUERY)
-			.dataSource(dataSource)
+		auth
+            .userDetailsService(thinkerDetailsService())
 			.passwordEncoder(bCryptPasswordEncoder);
 	}
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception{
-		http.authorizeRequests()
-        	.antMatchers("/css/**", "/fonts/**", "/js/**", "/images/**", "/webjars/**").permitAll()
-			.antMatchers("/", "/login").permitAll()
-			.antMatchers("/register", "/categories/**","/modeles/**","/processus/**","/criteres/**").hasAuthority(ADMIN)
-			.antMatchers("/dashboard/**","/risques/**").hasAnyAuthority(ADMIN,USER)
+		http
+            .authorizeRequests()
+			.antMatchers("/", "/register", "/login").permitAll()
+			.antMatchers("/ideas/**").hasAnyAuthority(USER,ADMIN)
 			.anyRequest().authenticated()
 			.and().csrf().disable()
-			.formLogin().loginPage("/login").failureUrl("/login?error=true")
-			.defaultSuccessUrl("/dashboard")
-			.usernameParameter("code")
+			.formLogin()/*.loginPage("/login")*/.failureUrl("/login?error=true")
+			/*.defaultSuccessUrl("/dashboard")*/
+			.usernameParameter("email")
 			.passwordParameter("password")
 			.and().logout()
 			.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
 			.logoutSuccessUrl("/login")
 			.and().rememberMe()
 			.and().exceptionHandling().accessDeniedPage("/access_denied");
-	}
-	
-	@Bean
-	public PersistentTokenRepository persistentTokenRepository() {
-		JdbcTokenRepositoryImpl db = new JdbcTokenRepositoryImpl();
-		db.setDataSource(dataSource);
-	
-		return db;
 	}
 }

0a294a17d8702762aa191cf4a0b04f791ac4b95a
SALMAN <elfadili.salman@gmail.com>
2019-02-19 18:21:36 +0100

    Added authenticateThinker function and mapping it with '/login'

--- src/main/java/com/crazyideas/controllers/AuthController.java
+++ src/main/java/com/crazyideas/controllers/AuthController.java
@@ -1,4 +1,29 @@
 package com.crazyideas.controllers;
 
+import com.crazyideas.repositories.RoleRepository;
+import com.crazyideas.repositories.ThinkerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
 public class AuthController {
+
+    @Autowired
+    ThinkerRepository thinkerRepository;
+    @Autowired
+    RoleRepository roleRepository;
+
+//    @PostMapping("/login")
+//    public ResponseEntity<?> authenticateThinker(@RequestBody Thinker thinker) {
+//
+//        Authentication authentication = authenticationManager.authenticate(
+//                new UsernamePasswordAuthenticationToken(thinker.getEmail(),thinker.getPassword())
+//        );
+//
+//        SecurityContextHolder.getContext().setAuthentication(authentication);
+//
+//        return ResponseEntity.ok(thinker);
+//    }
 }
